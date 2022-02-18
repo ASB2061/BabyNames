541df65a04497ace4d641e2fc702ddba5be44765
David G. Wonnacott <davew@cs.haverford.edu>
2022-02-17 20:06:46 -0500

    Fresh from Jeová

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Your main code goes here. Remember to create the NameData, Node and DoublyLinkedList classes in separate files
+    }
+}
